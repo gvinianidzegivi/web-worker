1abd1399d2e8881a204921ad8eafe3582a7d2a00
Givi Gvinianidze <gvinianidzegivi@gmail.com>
2026-09-12 16:26:18 +0400

    feat: simple solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,41 @@
+import { useEffect, useRef } from "react";
 
+const btnStyle = {
+  background: 'none',
+  border: '2px solid black',
+  borderRadius: '3px',
+  padding: '5px',
+  margin: '5px',
+  fontWeight: 'bold'
+} as const;
 
 export const App = () => {
+  const workerRef = useRef<Worker | null>(null);
+
+  const handleCalculation = () => {
+    workerRef.current.postMessage('calc')
+  };
+
+  const handleChangeBG = () => {
+    let bodyBG = document.body.style.background;
+    document.body.style.background = bodyBG === 'red' ? 'white' : 'red';
+  };
+
+  useEffect(() => {
+    const worker = new Worker(new URL('./worker.ts', import.meta.url), { type: 'module' })
+    workerRef.current = worker;
+
+    worker.onmessage = (message: MessageEvent<number>) => {
+      alert(message.data)
+    }
+
+    return () => worker.terminate()
+  }, [])
 
   return (
     <>
-      App
+      <button type="button" style={btnStyle} onClick={handleCalculation}>Expencive Func</button>
+      <button type="button" style={btnStyle} onClick={handleChangeBG}>Change BG Func</button>
     </>
   )
 }
---
+++ src/worker.ts
@@ -0,0 +1,9 @@
+self.onmessage = (event: MessageEvent) => {
+    if (event.data === 'calc') {
+        let result = 0;
+        for (let i = 0; i < 1_000_000_000; i++) {
+          result += i;
+        }
+        self.postMessage(result);
+    }
+};
